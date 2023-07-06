04c8ed2e5733d564308d77ac70fe342ce5a0df1c
liushengkai <949919743@qq.com>
2023-07-06 16:43:54 +0800

    feat：install nodejs fs扩展

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log(123)
+const package = require('./package.json')
+const program = require('commander')
+const chalk = require('chalk')
+const figlet = require('figlet')
+const Inquirer = require('inquirer')
+
+program.name(package.name).usage(`<commander>[option]`).version(package.version)
+program.parse(process.argv)
+
+new Inquirer.prompt({
+  name: 'Vue',
+  type: 'checkbox',
+  message: '请选择您要添加的工程',
+  choices: [{ name: 'Babel' }, { name: 'TS' }],
+}).then((data) => {
+  console.log(data)
+})
+
+console.log("/r/n" + figlet.textSync("create over",{
+  font: 'Ghost',
+  horizontalLayout: 'default',
+  verticalLayout: 'default',
+  width: 120,
+  whitespaceBreak: true
+}))
